d0b7376a293a644304d7e7867aacd974ca0b4014
mestrinio <ebis.pedromartins@gmail.com>
2023-10-06 16:27:21 +0100

    criar main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+# Avaliação 1 PSR - Typing test
+# 12/10/2023
+# 1 - Beatriz Marques; 2 - Pedro Martins; 3 - Tiago Pereira
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
